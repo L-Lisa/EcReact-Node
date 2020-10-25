983d439021aaab8b14040642b6ce021321d2d744
BoGrusDev <bo@grus.zone>
2020-10-25 10:08:07 +0100

    weather  3

--- steg-21/model/weather-model.js
+++
@@ -1,7 +0,0 @@
-class WeatherModel {
-
-    async weatherLoad() {
-        let promise = await fetch('https://api.openweathermap.org/data/2.5/group?id=5128581,5368361,1850147,1220988,524901,1819729,2988506,3369157,2147714,3530597,2711537,2706619,2678129,2673875,2666670,2693301&lang=SE&appid=cd076890ec3552da2c0f46b5caea9530&units=metric');
-        return promise;
-    }
-}
--- steg-21/view/weather-view.js
+++
@@ -1,7 +0,0 @@
-class WeatherView {
-    
-    showWeather(data) {
-       
-    }
-
-}
